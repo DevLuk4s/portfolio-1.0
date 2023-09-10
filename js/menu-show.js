5a263ac55563b337fa2b46ceeda41a995eb7b4de
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-09-10 03:46:43 -0300

    atualizado portfólio

--- js/menu-show.js
+++ js/menu-show.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "img/hamburger.png";
+        document.querySelector('.icon').src = "img/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "img/close.png";
+        document.querySelector('.icon').src = "img/close_white_36dp.svg";
     }
 }
