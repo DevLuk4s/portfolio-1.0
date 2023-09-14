a203b33b9f4a1712ad2d4ceb93726e263767d6e2
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-09-14 08:24:06 -0300

    atualizado portfólio

---
+++ js/scrollreveal.js
@@ -0,0 +1,24 @@
+ScrollReveal({
+    distance: '60px',
+    duration: 1000,
+    delay: 500
+})
+
+ScrollReveal().reveal('.home', { 
+    delay: 500,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.habilidades', {
+    delay: 500,
+    origin: 'right'
+});
+
+ScrollReveal().reveal('.projetos', {
+    delay: 500,
+    Rotate:{
+        x:0,
+        y:0,
+        z:0
+    }
+});
