0bc9bf7be7561b761fadf80c4333607189e29f04
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-12-16 02:51:37 -0300

    update project

--- js/habilidades.js
+++ js/habilidades.js
@@ -3,7 +3,7 @@ const elementos = {
   react: document.querySelector(".habilidades-react"),
   html: document.querySelector(".habilidades-html"),
   css: document.querySelector(".habilidades-css"),
-  bootstrap: document.querySelector(".habilidades-bootstrap"),
+  tailwind: document.querySelector(".habilidades-tailwind"),
   node: document.querySelector(".habilidades-node"),
   github: document.querySelector(".habilidades-github"),
   git: document.querySelector(".habilidades-git"),
@@ -33,8 +33,8 @@ function obtenerDescripcion(lenguaje) {
       "React é uma biblioteca JavaScript amplamente utilizada para o desenvolvimento de interfaces de usuário interativas e reativas em aplicações web. Criada e mantida pelo Facebook, o React permite que os desenvolvedores construam componentes reutilizáveis que representam partes específicas da interface do usuário. Esses componentes podem ser atualizados de forma eficiente quando os dados ou o estado da aplicação mudam, sem a necessidade de recarregar a página inteira.",
     css: "CSS é uma linguagem de estilo em resumo, o CSS permite que os desenvolvedores definam como os elementos HTML devem ser exibidos em uma página da web, personalizando propriedades como cores, fontes, tamanhos de texto e espaçamento. Isso resulta em designs atraentes e na formatação visual de páginas da web, melhorando a experiência do usuário.",
     html: "HTML é uma linguagem de marcação usada para criar e estruturar documentos na web. Ela define a hierarquia e o conteúdo de uma página, permitindo a inclusão de links, imagens e multimídia. HTML é compatível com navegadores e serve como base para o desenvolvimento web, trabalhando junto com CSS para estilização e JavaScript para interatividade. É essencial para criar e apresentar conteúdo na internet.",
-    bootstrap:
-      "Bootstrap é um popular framework de código aberto para desenvolvimento de páginas da web e aplicativos responsivos. Ele fornece uma coleção de estilos, componentes e JavaScript pré-desenvolvidos que facilitam a criação de interfaces de usuário atraentes e funcionais. Aqui está um resumo do Bootstrap",
+    tailwind:
+      "Tailwind CSS é um framework de estilo para desenvolvimento web que utiliza uma abordagem de utilitários para construir interfaces de usuário. Em vez de fornecer componentes predefinidos, como alguns outros frameworks CSS, o Tailwind oferece uma variedade extensa de classes utilitárias que podem ser aplicadas diretamente no HTML para estilizar elementos.",
     node: "Em resumo, Node.js é uma plataforma de tempo de execução JavaScript que permite aos desenvolvedores criar aplicativos de servidor escaláveis e eficientes, bem como ferramentas de linha de comando. Sua natureza assíncrona e não-bloqueante é especialmente adequada para lidar com várias conexões simultâneas, tornando-o uma escolha popular para aplicativos em tempo real e de alto desempenho na web.",
     github:
       "O GitHub é uma plataforma de hospedagem e colaboração de código-fonte usada principalmente por desenvolvedores de software.",
